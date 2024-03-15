f2ca4ff5de73a82c23999a99a0f8aab947de2421
andriibilyanskii <andriibilyanskii@gmail.com>
2024-03-15 23:24:13 +0200

    login second screen

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,13 +1,18 @@
-import React, {CSSProperties} from 'react';
+/* eslint-disable no-useless-escape */
+
+import React, {CSSProperties, useCallback, useState} from 'react';
 import classNames from 'classnames';
 
 import styles from './Input.module.scss';
+import {Icon} from "../../index";
+import {CONSTANTS} from "../../../constants";
 
 interface IProps {
     value: string;
     onChange: (e: string) => void;
     placeholder?: string;
     className?: string;
+    classNameContainer?: string;
     padding?: string;
     style?: CSSProperties;
     maxLength?: number;
@@ -15,19 +20,43 @@ interface IProps {
 }
 
 const Input: React.FC<IProps> = (props) => {
-    const {onChange, className, ...rest} = props;
+    const {onChange, className, classNameContainer, type, ...rest} = props;
+
+    const [showPassword, setShowPassword] = useState(false);
+    const showPasswordFunc = useCallback(() => {
+        setShowPassword(!showPassword);
+    }, [showPassword]);
 
     return (
-        <input
+        <label
             className={classNames({
-                [styles.input]: true,
-                [className || '']: Boolean(className),
+                [styles['input-container']]: true,
+                [classNameContainer || '']: !!classNameContainer,
             })}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
-                onChange(event?.target?.value);
-            }}
-            {...rest}
-        />
+        >
+            <input
+                className={classNames({
+                    [styles.input]: true,
+                    [className || '']: Boolean(className),
+                })}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+                    onChange(event?.target?.value);
+                }}
+                type={!showPassword ? type : 'text'}
+                {...rest}
+            />
+
+            {props?.type === 'password' && (
+                <Icon
+                    className={classNames({
+                        [styles['inputIcon']]: true,
+                    })}
+                    src={CONSTANTS.ICONS.eye}
+                    onClick={showPasswordFunc}
+                    size={'1.5rem'}
+                />
+            )}
+        </label>
     );
 };
 
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,17 @@
 import React, {FormEvent, useState} from 'react';
 
-import {Text, AuthLayout, Link, Button, Icon, Input} from 'components';
+import {Text, AuthLayout, Link, Button, Icon, Input, AnimateHeight} from 'components';
 import {CONSTANTS} from "../../constants";
+
+import {validateEmail} from 'utils'
 
 import styles from './LoginPage.module.scss';
 
 const LoginPage: React.FC = () => {
-    const [email, setEmail] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPasswordField, setShowPasswordField] = useState(false)
+
 
     return (
         <AuthLayout title={'Log in to your account'}>
@@ -30,14 +35,35 @@ const LoginPage: React.FC = () => {
             <form
                 onSubmit={(e: FormEvent<HTMLFormElement>) => {
                     e?.preventDefault();
+
+                    console.log(email, password)
                 }}
             >
                 <Input
                     value={email}
-                    onChange={setEmail}
+                    onChange={(value: string) => {
+                        setEmail(value);
+                        setShowPasswordField(validateEmail(email));
+                    }}
                     placeholder={'Work email'}
                     type={'email'}
+                    style={{
+                        marginBottom: showPasswordField ? "25px" : 0
+                    }}
                 />
+
+                <AnimateHeight isOpen={showPasswordField}>
+                    <Input
+                        value={password}
+                        onChange={setPassword}
+                        placeholder={'Password'}
+                        type={'password'}
+                    />
+
+                    <Text type={'b2'} className={styles['loginPage-forgotPasswordText']}>
+                        <Link to={'/auth/forgot-password'}>Forgot your password?</Link>
+                    </Text>
+                </AnimateHeight>
 
                 <Button type={'submit'}>Log in to Qencode</Button>
             </form>
