ce55322ac9690c6a1a4af2f852a0d7810df1b621
andriibilyanskii <andriibilyanskii@gmail.com>
2024-03-16 21:27:36 +0200

    fix cancel button

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,76 +1,76 @@
-import React, { FormEvent, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {FormEvent, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
-import { AuthLayout, Button, Input } from 'components';
+import {AuthLayout, Button, Input} from 'components';
 
-import { fetchData, useAppContext, validateEmail } from 'utils';
-import { IErrorData } from 'interfaces';
+import {fetchData, useAppContext, validateEmail} from 'utils';
+import {IErrorData} from 'interfaces';
 
 const ForgotPassword: React.FC = () => {
-	const { email, setEmail } = useAppContext();
-	const [isLoading, setIsLoading] = useState(false);
-	const history = useNavigate();
-	const [errorData, setErrorData] = useState<Array<IErrorData>>([]);
+    const {email, setEmail} = useAppContext();
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useNavigate();
+    const [errorData, setErrorData] = useState<Array<IErrorData>>([]);
 
-	useEffect(() => {
-		setErrorData([]);
-	}, [email]);
+    useEffect(() => {
+        setErrorData([]);
+    }, [email]);
 
-	return (
-		<AuthLayout title={'Forgot Password?'}>
-			<form
-				onSubmit={async (e: FormEvent<HTMLFormElement>) => {
-					e?.preventDefault();
+    return (
+        <AuthLayout title={'Forgot Password?'}>
+            <form
+                onSubmit={async (e: FormEvent<HTMLFormElement>) => {
+                    e?.preventDefault();
 
-					const response = await fetchData(
-						'/auth/password-reset',
-						true,
-						{
-							body: {
-								email,
-							},
-						},
-						{
-							setIsLoading,
-							showSuccessMessage: true,
-						}
-					);
+                    const response = await fetchData(
+                        '/auth/password-reset',
+                        true,
+                        {
+                            body: {
+                                email,
+                            },
+                        },
+                        {
+                            setIsLoading,
+                            showSuccessMessage: true,
+                        }
+                    );
 
-					if (response?.status === 422 && typeof response?.detail !== 'string') {
-						setErrorData(response?.detail);
-					}
-					// Because the server returns "Invalid user", this condition is omitted
-					// else {
-					history('/auth/create-password');
-					// }
-				}}
-			>
-				<Input
-					value={email}
-					onChange={setEmail}
-					placeholder={'Enter your email'}
-					type={'email'}
-					name={'email'}
-					disabled={isLoading}
-					errorMessage={!validateEmail(email) && email ? 'Wrong email format' : ''}
-					errorData={errorData}
-				/>
+                    if (response?.status === 422 && typeof response?.detail !== 'string') {
+                        setErrorData(response?.detail);
+                    }
+                    // Because the server returns "Invalid user", this condition is omitted
+                    // else {
+                    history('/auth/create-password');
+                    // }
+                }}
+            >
+                <Input
+                    value={email}
+                    onChange={setEmail}
+                    placeholder={'Enter your email'}
+                    type={'email'}
+                    name={'email'}
+                    disabled={isLoading}
+                    errorMessage={!validateEmail(email) && email ? 'Wrong email format' : ''}
+                    errorData={errorData}
+                />
 
-				<Button type={'submit'} disabled={!validateEmail(email)}>
-					Send
-				</Button>
+                <Button type={'submit'} disabled={!validateEmail(email)}>
+                    Send
+                </Button>
 
-				<Button
-					buttonType={'transparent'}
-					onClick={() => {
-						history(-1);
-					}}
-				>
-					Cancel
-				</Button>
-			</form>
-		</AuthLayout>
-	);
+                <Button
+                    buttonType={'transparent'}
+                    onClick={() => {
+                        history('/auth/login');
+                    }}
+                >
+                    Cancel
+                </Button>
+            </form>
+        </AuthLayout>
+    );
 };
 
 export default ForgotPassword;
